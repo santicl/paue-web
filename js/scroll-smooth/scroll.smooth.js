96e6614e0e3685206a0df8a0b366cb6cb1a6ce4f
santicl <caballerosuarezsantiago@gmail.com>
2023-12-01 14:57:10 -0500

    Primer commit

---
+++ js/scroll-smooth/scroll.smooth.js
@@ -0,0 +1,42 @@
+document.getElementById('mouse-fixed').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('mouse-fixed-two').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('mouse-fixed-three').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('mouse-fixed-four').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('mouse-fixed-five').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('mouse-fixed-six').addEventListener('click', () => {
+    console.log('scroll next')
+    const nextSection = document.getElementById('next-section');
+
+    nextSection.scrollIntoView({ behavior: 'smooth' });
+});
+
